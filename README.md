208d7412954805e193917673d6bfe42decd3581a
MosQyy <akbarma2002@gmail.com>
2021-09-30 19:28:13 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter Apps (8)
+## Rubiks Tutorial (How to Solve Rubiks) Mobile App
+Preview :
---
+++ android/app/src/main/kotlin/com/example/rubiks_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rubiks_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
